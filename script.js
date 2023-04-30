eae10e1b31dd05efeee0af869223fa82e9bbb069
RostyslavBaranets <baranets.01@gmail.com>
2023-04-30 20:40:46 +0300

    feat: add tab, caps lock, space, backspace, del, enter

--- script.js
+++ script.js
@@ -7,22 +7,64 @@ document.body.appendChild(textArea);
 const keyboard = document.createElement('div');
 document.body.appendChild(keyboard);
 
-const keyss = keys.en.lowercase;
+let isCapsLock = false;
 
-keyss.forEach(row => {
-    const rowKey = document.createElement('div');
-    row.forEach(key => {
-        const button = document.createElement('button');
-        button.classList.add('keyboard__btn')
-        button.textContent = key;
-        rowKey.appendChild(button);
+addKeys("lowerCase");
+
+function addKeys(cases){
+    keyboard.innerHTML = "";
+
+    keys.en[cases].forEach(row => {
+        const rowKey = document.createElement('div');
+        row.forEach(key => {
+            const button = document.createElement('button');
+            button.classList.add('keyboard__btn')
+            button.textContent = key;
+            rowKey.appendChild(button);
+        })
+        keyboard.appendChild(rowKey);
     })
-    keyboard.appendChild(rowKey);
-})
+    addFunk();
+}
 
+function addFunk(){
 const buttons = document.querySelectorAll('.keyboard__btn');
 buttons.forEach(btn => {
     btn.addEventListener('click', () => {
-        textArea.value += btn.textContent;
+        const cursor = textArea.selectionEnd;
+        switch(btn.textContent){
+            case 'tab': textArea.value += '    ';
+                break;
+            case 'caps lock': 
+                isCapsLock = !isCapsLock;
+                if(isCapsLock == false){
+                    addKeys("lowerCase");
+                }else{
+                    addKeys("upperCase");
+                }
+                break;
+            case 'shift': textArea.value += '';
+                break;
+            case 'space': textArea.value += ' ';
+                break;
+            case 'backspace': textArea.value = textArea.value.slice(0, -1);
+                break;
+            case 'del': 
+                if(cursor < textArea.value.length){  
+                    textArea.value = textArea.value.slice(0, cursor) + textArea.value.slice(cursor + 1);
+                    textArea.focus();
+                    textArea.selectionEnd = cursor;
+                }
+                break;
+            case 'enter': textArea.value += '\n';
+                break;
+            default: 
+                textArea.value = textArea.value.slice(0, cursor) + btn.textContent + textArea.value.slice(cursor);
+                textArea.focus();
+                textArea.selectionEnd = cursor + 1;
+                break;
+        }   
     })
 })
+}
+
